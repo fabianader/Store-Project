0b076739ba714868fb48cc76205c4d30cde57435
Fabian Ader <fabian.ader6@gmail.com>
2026-02-18 17:14:08 +0330

    .

--- Common/Directories.cs
+++ Common/Directories.cs
@@ -5,8 +5,10 @@
         public const string UserProfilePicture = "wwwroot/images/users";
         public const string ProductImage = "wwwroot/images/products";
         public const string IconImage = "wwwroot/images/icons";
+        public const string BannerImage = "wwwroot/images/banners";
         public static string GetUserProfilePicture(string ProfilePicture) => $"{UserProfilePicture.Replace("wwwroot", "")}/{ProfilePicture}";
         public static string GetProductImage(string ProductImageUrl) => $"{ProductImage.Replace("wwwroot", "")}/{ProductImageUrl}";
         public static string GetIconImage(string IconImageUrl) => $"{IconImage.Replace("wwwroot", "")}/{IconImageUrl}";
+        public static string GetBannerImage(int BannerImageNumber) => $"{BannerImage.Replace("wwwroot", "")}/{BannerImageNumber}.jpg";
     }
 }
